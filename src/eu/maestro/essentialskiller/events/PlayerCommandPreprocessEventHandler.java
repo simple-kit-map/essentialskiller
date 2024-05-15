ce49126e5a03bb123ef1d728b0540e85ee7d7753
Couleur <82747632+couleurm@users.noreply.github.com>
2024-05-15 03:35:09 +0200

    unsure what this does tbf

--- src/eu/maestro/essentialskiller/events/PlayerCommandPreprocessEventHandler.java
+++ src/eu/maestro/essentialskiller/events/PlayerCommandPreprocessEventHandler.java
@@ -5,7 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
-import net.minecraft.server.v1_8_R3.MinecraftServer;
+import net.minecraft.server.v1_7_R4.MinecraftServer;
 
 public class PlayerCommandPreprocessEventHandler implements Listener {
 	
@@ -14,7 +14,7 @@ public class PlayerCommandPreprocessEventHandler implements Listener {
 		
 		final Player player = event.getPlayer();
 		final String command = event.getMessage().replace("/", "");
-		
+		/*
 		if(command.equals("tps")) {
 			double[] recentTps = MinecraftServer.getServer().recentTps;
 			double totalTps = 0.0;
@@ -29,6 +29,7 @@ public class PlayerCommandPreprocessEventHandler implements Listener {
 			event.setCancelled(true);
 			return;
 		}
+		*/
 	}
 	
 }
